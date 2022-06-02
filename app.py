867823522d101cff1660972db86bc6479dd4886e
kartien <kevinramon523@gmail.com>
2022-06-02 16:11:57 -0500

    Finish model db

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from psycopg2 import connect
 
 app = Flask(__name__)
 
+
+host = 'localhost'
+port = 5432
+dbname = 'userdb'
+user = 'postgres'
+password = 'mysecretpassword'
+
+def get_connection():
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
+
+
 @app.get('/')
 def home():
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    result = cur.execute("SELECT 1 + 1")
+    print(result)
     return "Hello world"
+
 
 
 if __name__ == '__main__':
