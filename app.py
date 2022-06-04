bc6b7915ba617dddf22ed98abee7e224e44bd933
kartien <kevinramon523@gmail.com>
2022-06-03 22:41:33 -0500

    Model get post delete put

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from psycopg2 import connect
 
 app = Flask(__name__)
@@ -14,15 +14,27 @@ def get_connection():
     conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
     return conn
 
+@app.get('/api/users')
+def get_users():
+    return 'getting users'
 
-@app.get('/')
-def home():
-    conn = get_connection()
-    cur = conn.cursor()
-    
-    result = cur.execute("SELECT 1 + 1")
-    print(result)
-    return "Hello world"
+@app.post('/api/users')
+def create_user():
+    print(request.get_json())
+    return 'creating users'
+
+@app.delete('/api/users')
+def delete_user():
+    return 'deleting users'
+
+@app.put('/api/users/1')
+def update_user():
+    return 'udatting users 1'
+
+@app.get('/api/users/1')
+def get_user():
+    return 'getting users 1'
+
 
 
 
