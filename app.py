cedd77f441655b39a9694d228a1eff0d50d59327
kartien <kevinramon523@gmail.com>
2022-06-04 22:54:41 -0500

    add model database

--- app.py
+++ app.py
@@ -6,39 +6,54 @@ app = Flask(__name__)
 
 host = 'localhost'
 port = 5432
-dbname = 'userdb'
+dbname = 'dbuser'
 user = 'postgres'
 password = 'mysecretpassword'
 
+
 def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    conn = connect(host=host, port=port, dbname=dbname,
+                   user=user, password=password)
     return conn
+
 
 @app.get('/api/users')
 def get_users():
     return 'getting users'
 
+
 @app.post('/api/users')
 def create_user():
+    new_user = request.get_json()
+    username = new_user['username']
+    email = new_user['email']
+    password = new_user['password']
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)',
+                (username, email, password))
+    conn.commit()
+    cur.close()
+    conn.close()
     print(request.get_json())
     return 'creating users'
+
 
 @app.delete('/api/users')
 def delete_user():
     return 'deleting users'
 
+
 @app.put('/api/users/1')
 def update_user():
     return 'udatting users 1'
+
 
 @app.get('/api/users/1')
 def get_user():
     return 'getting users 1'
 
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
